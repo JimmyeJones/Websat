13b2ab4f4f46b525c2c1fd28884f0ac965280dff
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-08-07 07:26:40 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -37,7 +37,7 @@ all_image_paths = get_image_paths()
 
 image_paths = []
 for path in all_image_paths:
-    if req_1 in path and req_2 in path:
+    if req_1, req_2 in path:
         image_paths.append(path)
 
 st.write(f"Found {len(image_paths)} images.")
