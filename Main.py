b5a459e77e2fd3302e328885cb2d1541949c8063
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-08-18 11:03:18 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ st.set_page_config(
 st.markdown(
     """
     <style>
-    .st-emotion-cache-mnu3yk.ef3psqc6, .viewerBadge_link__qRIco, .st-emotion-cache-mnu3yk.ef3psqc6 {
+    .st-emotion-cache-mnu3yk.ef3psqc6, .viewerBadge_link__qRIco, .st-emotion-cache-mnu3yk.ef3psqc6, .viewerBadge_link__qRIco {
         display: none;
     }
     </style>
