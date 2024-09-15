27131c11c45fbb307e94e528163086aaf6adc7aa
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-09-15 13:51:50 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ st.markdown(
     
 #Notifications
 if True:
-    st.warning("Scheduled maintenance: 2-5PM EST September 15 - maintenance will fix image decoder issues. The image viewer will be unresponsive.")
+    st.warning("Scheduled maintenance: 2-5PM EST September 15 - maintenance will fix image decoder issues. The image viewer will be unresponsive during this time.")
     
     #end of notifications
     
